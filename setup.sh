7618f3662dcbb75d91d50137533a6d6e6f3f4619
Steven Lang <steven.lang.mz@gmail.com>
2018-01-21 18:36:47 +1300

    Fix typo

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -e
 
 name=$1
 virtualenv3 --python=python3 env
-./env/bin/pip install -r requirements
+./env/bin/pip install -r requirements.txt
 
 mv botname ${name}
 sed "s/botname/${name}/g" main.py
